05c9524a306f749fc46927388b1e906ef6e2f69d
Nho Nam Đinh <nhonamstg@gmail.com>
2022-04-11 19:58:04 +0700

    bo sung view admin

--- src/main/java/spring/controller/admin/QLHoaDon.java
+++ src/main/java/spring/controller/admin/QLHoaDon.java
@@ -1,18 +1,35 @@
 package spring.controller.admin;
 
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import spring.entity.BanEntity;
+
 @Controller
+@Transactional
 public class QLHoaDon {
 
+	@Autowired
+	SessionFactory factory;
 	@RequestMapping(value = "admin-hoadon" , method = RequestMethod.GET)
-	public String showMenu() {
+	public String showMenu(ModelMap model) {
+		Session session = factory.getCurrentSession();
+		String hql = "FROM HoaDonEntity";
+		Query query = session.createQuery(hql);
+		List<BanEntity> list = query.list();
+		model.addAttribute("hoadon", list);
 		return "admin/qlhoadon";
 	}
-	
-  
 
 }
